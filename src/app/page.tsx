fb532f6dad999398607b841fa40ea12bc36dd3cc
Rawand <info.rawandkhalid@gmail.com>
2025-03-31 22:26:29 +0200

    '_'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,67 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="p-6 md:p-12 gap-4 grid relative scroll-smooth h-dvh overflow-auto [scroll-snap-type:_y_mandatory]">
+      <section className="gap-3 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl text-zinc-900">
+          hi, i{"'"}m rawand
+        </h1>
+        <p className="text-xl md:text-2xl">
+          I{"'"}m a 19-year-old web developer based in Germany 🇩🇪, specializing
+          in AI-powered products.
+        </p>
+        <p className="text-xl md:text-2xl">
+          <span className="font-bold text-zinc-800">My tech stack:</span>{" "}
+          Appwrite, Google Cloud, Next.js, Figma, Notion
+        </p>
+        <p className="text-xl md:text-2xl">
+          I believe the best products are simple, yet high-quality!
+        </p>
+      </section>
+      <section className="gap-3 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+        <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-zinc-900">
+          Let{"'"}s connect!
+        </h2>
+        <p className="text-xl md:text-2xl">
+          The fastest way to reach me is on X{" "}
+          <Link
+            href="https://twitter.com/rawand.tech"
+            className="font-bold text-blue-600 underline hover:text-blue-800 transition-colors"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            @rawand.tech
+          </Link>{" "}
+          or Instagram{" "}
+          <Link
+            href="https://instagram.com/rawand.tech"
+            className="font-bold text-red-600 underline hover:text-red-800 transition-colors"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            @rawand.tech
+          </Link>
+          .
+        </p>
+        <p className="text-xl md:text-2xl">
+          Shoot me a message at:{" "}
+          <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
+            hello@rawand.tech
+          </code>
+        </p>
+      </section>
+      <section className="gap-3 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+        <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-zinc-900">
+          🚀 Building a SaaS tool?
+        </h2>
+        <p className="text-xl md:text-2xl">
+          If you{"'"}re working on something exciting and need a hand taking it
+          to the next level, I{"'"}d love to help.
+        </p>
+        <p className="text-xl md:text-2xl">
+          Drop me an email at:{" "}
+          <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
+            help@rawand.tech
+          </code>
+        </p>
+      </section>
     </div>
   );
 }
