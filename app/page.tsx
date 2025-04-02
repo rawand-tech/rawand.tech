a7a355ed8940a51755fe1cb033333fb4d6cab735
itsR4wand <info.rawandkhalid@gmail.com>
2025-04-02 09:25:28 +0200

    minify codes

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="p-6 md:p-12 gap-4 grid relative scroll-smooth h-dvh overflow-auto [scroll-snap-type:_y_mandatory]">
-      <section className="gap-3 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
-        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl text-zinc-900">
+    <div className="lowercase p-6 md:p-12 gap-4 grid relative scroll-smooth h-dvh overflow-auto [scroll-snap-type:_y_mandatory]">
+      <section className="gap-4 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+        <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl text-zinc-900">
           hi, i{"'"}m rawand
         </h1>
         <p className="text-xl md:text-2xl">
@@ -13,14 +13,11 @@ export default function HomePage() {
         </p>
         <p className="text-xl md:text-2xl">
           <span className="font-bold text-zinc-800">My tech stack:</span>{" "}
-          Appwrite, Google Cloud, Next.js, Figma, Notion
-        </p>
-        <p className="text-xl md:text-2xl">
-          I believe the best products are simple, yet high-quality!
+          Appwrite, Google Cloud, Next.js, Figma, Notion.
         </p>
       </section>
-      <section className="gap-3 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
-        <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-zinc-900">
+      <section className="gap-4 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+        <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl text-zinc-900">
           Let{"'"}s connect!
         </h2>
         <p className="text-xl md:text-2xl">
@@ -41,14 +38,14 @@ export default function HomePage() {
           .
         </p>
         <p className="text-xl md:text-2xl">
-          Shoot me a message at:{" "}
+          shoot me a message at:{" "}
           <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
             hello@rawand.tech
           </code>
         </p>
       </section>
-      <section className="gap-3 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
-        <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-zinc-900">
+      <section className="gap-4 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+        <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl text-zinc-900">
           🚀 Building a SaaS tool?
         </h2>
         <p className="text-xl md:text-2xl">
