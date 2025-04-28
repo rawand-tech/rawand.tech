bb680eed4bdf39204efa2cc0389472d394d7d7fd
Rawand <info.rawandkhalid@gmail.com>
2025-04-28 20:14:18 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,34 +4,27 @@ import "./globals.css";
 import Script from "next/script";
 
 const inter = Inter({
-  subsets: ["latin"],
-  weight: ["500", "700"],
-  display: "swap",
+    subsets: ["latin"],
+    weight: ["500", "700"],
+    display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Rawand.Tech",
-  description:
-    "I am a 19-year-old web developer in DE🇩🇪 that builds AI Products.",
+    title: "Rawand.Tech",
+    description: "I am a 19-year-old web developer in DE🇩🇪 that builds AI Products.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" dir="ltr">
-      <body
-        className={`${inter.className} antialiased text-2xl font-medium tracking-tight text-zinc-500 bg-zinc-100`}
-      >
-        {children}
-        <Script
-          src="https://cloud.umami.is/script.js"
-          data-website-id="ac902327-521e-4412-99ac-2ff5cce71237"
-          defer
-        ></Script>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" dir="ltr">
+            <body className={`${inter.className} antialiased text-2xl font-medium tracking-tight text-zinc-500 bg-zinc-100`}>
+                {children}
+                <Script src="https://cloud.umami.is/script.js" data-website-id="ac902327-521e-4412-99ac-2ff5cce71237" defer></Script>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,69 @@
 import Link from "next/link";
 
 export default function HomePage() {
-  return (
-    <div className="lowercase p-6 md:p-12 gap-4 grid relative scroll-smooth h-dvh overflow-auto [scroll-snap-type:_y_mandatory]">
-      <Section title="my name is rawand">
-        <p className="text-xl md:text-2xl">
-          I{"'"}m a 19-year-old web developer based in Germany 🇩🇪, specializing
-          in AI-powered products.
-        </p>
-        <p className="text-xl md:text-2xl">
-          I am currently building an app called BewerbAI.
-        </p>
-        <p className="text-xl md:text-2xl">
-          <span className="font-bold text-zinc-800">My tech stack:</span>{" "}
-          Appwrite, Google Cloud, Python, JS/TS, nextjs, Figma, Notion
-        </p>
-      </Section>
-      <Section title="Let's connect!">
-        <p className="text-xl md:text-2xl">
-          The fastest way to reach me is on X{" "}
-          <Link
-            href="https://twitter.com/rawand.tech"
-            className="font-bold text-blue-600 underline hover:text-blue-800 transition-colors"
-          >
-            @rawand.tech
-          </Link>{" "}
-          or Instagram{" "}
-          <Link
-            href="https://instagram.com/rawand.tech"
-            className="font-bold text-red-600 underline hover:text-red-800 transition-colors"
-          >
-            @rawand.tech
-          </Link>
-          .
-        </p>
-        <p className="text-xl md:text-2xl">
-          shoot me a message at:{" "}
-          <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
-            hello@rawand.tech
-          </code>
-        </p>
-      </Section>
-      <Section title="🚀 Building a SaaS tool?">
-        <p className="text-xl md:text-2xl">
-          If you{"'"}re working on something exciting and need a hand taking it
-          to the next level, I{"'"}d love to help.
-        </p>
-        <p className="text-xl md:text-2xl">
-          Drop me an email at:{" "}
-          <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
-            help@rawand.tech
-          </code>
-        </p>
-      </Section>
-      <Section>
-        <p className="sign-font text-center capitalize text-5xl font-bold text-zinc-500">
-          Rawand
-        </p>
-      </Section>
-    </div>
-  );
+    return (
+        <div className="lowercase p-6 md:p-12 gap-4 grid relative scroll-smooth h-dvh overflow-auto [scroll-snap-type:_y_mandatory]">
+            <Section title="my name is rawand">
+                <p className="text-xl md:text-2xl">
+                    I{"'"}m a 19-year-old web developer based in Germany 🇩🇪, specializing in AI-powered products.
+                </p>
+                <p className="text-xl md:text-2xl">I am currently building an app called bewerb.ai.</p>
+                <p className="text-xl md:text-2xl">
+                    <span className="font-bold text-zinc-800">My tech stack:</span> Appwrite, Google Cloud, Python, JS/TS, nextjs, Figma,
+                    Notion
+                </p>
+            </Section>
+            <Section title="Let's connect!">
+                <p className="text-xl md:text-2xl">
+                    The fastest way to reach me is on X{" "}
+                    <Link
+                        href="https://twitter.com/rawand_tech"
+                        className="font-bold text-blue-600 underline hover:text-blue-800 transition-colors"
+                    >
+                        @rawand.tech
+                    </Link>{" "}
+                    or Instagram{" "}
+                    <Link
+                        href="https://instagram.com/rawand.tech"
+                        className="font-bold text-red-600 underline hover:text-red-800 transition-colors"
+                    >
+                        @rawand.tech
+                    </Link>
+                    .
+                </p>
+                <p className="text-xl md:text-2xl">
+                    shoot me a message at:{" "}
+                    <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
+                        hello@rawand.tech
+                    </code>
+                </p>
+            </Section>
+            <Section title="🚀 Building a SaaS tool?">
+                <p className="text-xl md:text-2xl">
+                    If you{"'"}re working on something exciting and need a hand taking it to the next level, I{"'"}d love to help.
+                </p>
+                <p className="text-xl md:text-2xl">
+                    Drop me an email at:{" "}
+                    <code className="cursor-copy text-zinc-900 underline hover:bg-zinc-200 transition-colors p-1 rounded">
+                        help@rawand.tech
+                    </code>
+                </p>
+            </Section>
+        </div>
+    );
 }
 
 function Section({
-  children,
-  title,
+    children,
+    title,
 }: Readonly<{
-  children: React.ReactNode;
-  title?: string;
+    children: React.ReactNode;
+    title?: string;
 }>) {
-  return (
-    <section className="gap-4 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
-      {title && (
-        <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl text-zinc-900">
-          {title}
-        </h2>
-      )}
-      {children}
-    </section>
-  );
+    return (
+        <section className="gap-4 flex flex-col justify-center w-full max-w-7xl mx-auto h-full min-h-dvh [scroll-snap-align:_start] px-4 sm:px-6">
+            {title && <h2 className="font-bold text-2xl md:text-3xl xl:text-4xl text-zinc-900">{title}</h2>}
+            {children}
+        </section>
+    );
 }
